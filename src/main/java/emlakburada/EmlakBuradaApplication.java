30466ff888f5d1ba9192abd6669e7ac623555010
Cem Dırman <cemdrman@gmail.com>
2022-02-26 19:24:09 +0300

    service discovery client

--- src/main/java/emlakburada/EmlakBuradaApplication.java
+++ src/main/java/emlakburada/EmlakBuradaApplication.java
@@ -2,10 +2,10 @@ package emlakburada;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {})
+@EnableEurekaClient
 public class EmlakBuradaApplication {
 
 	public static void main(String[] args) {
